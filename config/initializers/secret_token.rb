0c10a9387d94ca206bc18a84400fecac8e66bae1
Cory Stephenson <firechild7976@gmail.com>
2012-09-02 18:42:27 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ScheduleSearch::Application.config.secret_token = '7f827a6dc4153169ab5041e215440a4019f438cb9fef42fc7469085ebc46a67ca9bf6bc1141c47caf1ac3d058209b00a16fc35de87d9f32eb93e29f00a92a977'
